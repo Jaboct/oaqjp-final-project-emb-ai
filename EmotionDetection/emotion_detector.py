1efd1f12bb0db29c850695138fae8aa61b4c0834
Jaboct <jaboct@jaboct.com>
2024-01-18 11:15:34 -0500

    done with task 1 through 5

---
+++ EmotionDetection/emotion_detector.py
@@ -0,0 +1,45 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyse):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    myobj = { "raw_document": { "text": text_to_analyse } }
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    response = requests.post(url, json=myobj, headers=headers)
+
+    formatted_response = response.json()
+    emotions = formatted_response["emotionPredictions"][0]["emotion"];
+
+    domEmotion = get_dominant_emotion(emotions);
+    output = {
+        "anger":emotions["anger"],
+        "disgust":emotions["disgust"],
+        "fear":emotions["fear"],
+        "joy":emotions["joy"],
+        "sadness":emotions["sadness"],
+        "dominant_emotion":domEmotion
+    }
+    return output;
+
+
+def get_dominant_emotion(emotions):
+    check = [
+        "anger",
+        "disgust",
+        "fear",
+        "joy",
+        "sadness"
+    ]
+
+    dom = ""
+    domVal = 0
+
+    for name in check:
+        if ( emotions[name] > domVal ):
+            dom = name;
+            domVal = emotions[name];
+    return dom;
+
+
+
+
